4417b655d213758a4a70d01566b3a7ea2d570206
Jorge Ruiz <97487049+jorgeruizm94@users.noreply.github.com>
2022-10-30 22:41:23 -0700

    Page Creation
    Each page was created and linked to the app component.
    
    NavBar file was created yet not linked.

---
+++ moviefy-website/src/ExplorePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ExplorePage() {
+    return (
+        <div>
+            <h1>Movies</h1>
+        </div>
+    );
+}
+
+export default ExplorePage;
---
+++ moviefy-website/src/UserPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UserPage() {
+    return (
+        <div>
+            <h1>User's Movies</h1>
+        </div>
+    );
+}
+
+export default UserPage;
